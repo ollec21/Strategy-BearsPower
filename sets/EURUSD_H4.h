0f47be987078959d0e2cd32e302152633725ec35
kenorb <kenorb@users.noreply.github.com>
2020-01-09 17:39:31 +0000

    Updates strategy code up to latest standard

---
+++ sets/EURUSD_H4.h
@@ -0,0 +1,26 @@
+//+------------------------------------------------------------------+
+//|                  EA31337 - multi-strategy advanced trading robot |
+//|                       Copyright 2016-2020, 31337 Investments Ltd |
+//|                                       https://github.com/EA31337 |
+//+------------------------------------------------------------------+
+
+// Defines strategy's parameter values for the given pair symbol and timeframe.
+struct Stg_BearsPower_EURUSD_H4_Params : Stg_BearsPower_Params {
+  Stg_BearsPower_EURUSD_H4_Params() {
+    symbol = "EURUSD";
+    tf = PERIOD_H4;
+    BearsPower_Period = 13;
+    BearsPower_Applied_Price = 1;
+    BearsPower_Shift = 0;
+    BearsPower_TrailingStopMethod = 0;
+    BearsPower_TrailingProfitMethod = 0;
+    BearsPower_SignalOpenLevel = 0;
+    BearsPower_SignalBaseMethod = 0;
+    BearsPower_SignalOpenMethod1 = 0;
+    BearsPower_SignalOpenMethod2 = 0;
+    BearsPower_SignalCloseLevel = 0;
+    BearsPower_SignalCloseMethod1 = 0;
+    BearsPower_SignalCloseMethod2 = 0;
+    BearsPower_MaxSpread = 0;
+  }
+};
