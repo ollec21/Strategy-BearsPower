45c7068786acbfb40d10843d2732a8e50e32dcf9
kenorb <kenorb@users.noreply.github.com>
2020-12-31 03:22:20 +0000

    Optimize SignalOpenMethod (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -13,7 +13,7 @@ struct Stg_bears_Params_M5 : StgParams {
   // Struct constructor.
   Stg_bears_Params_M5() : StgParams(stg_bears_defaults) {
     lot_size = 0;
-    signal_open_method = 0;
+    signal_open_method = -1;
     signal_open_filter = 1;
     signal_open_level = 0;
     signal_open_boost = 0;
